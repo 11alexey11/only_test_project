10e80c279b6cf7bef85df1d9b71ca4796e8954ec
Alexey Yanvarev <yanvarev_2015@mail.ru>
2022-08-18 14:59:44 +0400

    feat: Setup environment

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+
+    if (isProd) {
+        config.minimizer = [
+            new TerserWebpackPlugin()
+        ]
+    }
+
+    return config;
+};
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    entry: path.resolve(__dirname, './src/index.tsx'),
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'build'),
+        libraryExport: 'default'
+    },
+    optimization: optimization(),
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
+    devServer: {
+        port: 5000,
+        hot: isDev
+    },
+    devtool: isDev ? 'source-map' : 'eval',
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    },
+                    'eslint-loader'
+                ],
+            }
+        ],
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+    ]
+};
